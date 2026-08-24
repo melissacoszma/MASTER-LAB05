f586b24591288717f20fe4592b6f0827bd723a6f
Melisa <melisacozma1234@gmail.com>
2026-08-24 13:31:20 +0200

    finalizado con reto

--- angular1/angular-lab/src/app/services/auth.service.ts
+++ angular1/angular-lab/src/app/services/auth.service.ts
@@ -8,10 +8,19 @@ export class AuthService {
   private loggedIn = false; // inicializacion 
   private username = '';
 
+  constructor() {
+  this.loggedIn = localStorage.getItem('isLogged') === 'true'; // al arrancar lee el estado guardado y si existia una sesion privada la recupera
+  this.username = localStorage.getItem('username') ?? '';
+}
+
   login(credentials: { username: string, password: string }): boolean {
     if (credentials.username === 'master@lemoncode.net' && credentials.password === '12345678') {
       this.loggedIn = true;
       this.username = credentials.username;
+
+      localStorage.setItem('isLogged', 'true');       // guarda el estado en localstorage
+      localStorage.setItem('username', credentials.username);
+
       return true;
     }
     return false;
@@ -20,6 +29,10 @@ export class AuthService {
   logout(): void {
     this.loggedIn = false; 
     this.username = '';
+
+    localStorage.removeItem('isLogged'); // limpia localstorage explicitamente al salir
+    localStorage.removeItem('username');
+
   }
 
   isLogged(): boolean {
